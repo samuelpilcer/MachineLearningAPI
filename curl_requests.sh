6c879e6bbb49d1876802c7a99aa6436c8cc33517
Valérie <samuelpilcer@gmail.com>
2018-01-07 19:49:05 +0100

    Useful curl requests

--- curl_requests.sh
+++ curl_requests.sh
@@ -1,3 +1,6 @@
-curl -i -H "Content-Type: application/json" -X POST -d '{"description":"Test", "inputs":784, "layers":[23,32,44]}' http://localhost:80/create
-curl -i -H "Content-Type: application/json" -X POST -d '{"description":"Test", "training_file":"training/training_mnist_0783-784.csv", "training_columns":784, "output_column":785}' http://localhost:80/train/2
-curl -i -H "Content-Type: application/json" -X POST -d '{"saving_file":"files/test_mnist"}' http://localhost:80/save/3
+curl -i -H "Content-Type: application/json" -X POST -d '{"layers":[13,13,45],"inputs":784,"outputs":10,"description":"Test"}' http://dnsaddress/create
+curl -X POST --form "file=@fileaddress.csv" http://dnsaddress/uploadtraining/3
+curl -i -H "Content-Type: application/json" -X POST -d '{"description":"Test", "training_file":"static/test.csv", "training_columns":784, "output_column":785, "epochs":1}' http://dnsaddress/train/3
+curl http://dnsaddress/save/3
+curl http://dnsaddress/download_str/3 -o Test.json
+curl http://dnsaddress/download_weights/3 -o Test.h5

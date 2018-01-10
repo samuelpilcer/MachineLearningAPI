310d5b0ab6b5a2a54fadd3d757eab7f46899c4a2
Valérie <samuelpilcer@gmail.com>
2018-01-10 19:26:06 +0100

    Fix bug

--- app.py
+++ app.py
@@ -191,6 +191,7 @@ def process_file(model_id):
     file=pd.read_csv("static/to_process/file_"+str(model_id)+".csv")
     res=[]
     for i in range(file.index.size):
+        print(i)
         res.append(list(file.iloc[i,:])+[models[model_id]["model"].predict(file.iloc[i,:])])
     pd.DataFrame(res).to_csv("static/processed/file_"+str(model_id)+".csv")
     return send_from_directory('static', "processed/file_"+str(model_id)+".csv")
